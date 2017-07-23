a28723b55251b9ca537b3a615e59558c716fc6cf
Bryan <bryan.p.connelly@gmail.com>
2017-07-23 11:29:10 -0500

    Allowed cross-origin requests from front end

--- src/main/java/com/bconnelly/gdax/notifier/controller/EthController.java
+++ src/main/java/com/bconnelly/gdax/notifier/controller/EthController.java
@@ -2,8 +2,9 @@ package com.bconnelly.gdax.notifier.controller;
 
 import com.bconnelly.gdax.notifier.representation.ETH_USD_MATCH;
 import com.bconnelly.gdax.notifier.service.EthService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,23 +13,26 @@ import java.util.List;
 /**
  * Created by Bryan on 7/20/2017.
  */
-
+@CrossOrigin(origins = "http://localhost:63343")
 @RestController
 public class EthController {
 
     EthService service = new EthService();
 
-    @RequestMapping("/getUpdates/{lastUpdate}")
+    @RequestMapping(value = "/getUpdates/{lastUpdate}", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ETH_USD_MATCH> getUpdates(@PathVariable String lastUpdate){
         System.out.println("pinged endpoint");
 
         return service.fetchMatchesSinceSequence(Integer.valueOf(lastUpdate));
     }
 
-    @RequestMapping("/getLastN/{nMatches}")
+    @RequestMapping(value = "/getLastN/{nMatches}", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ETH_USD_MATCH> getLastN(@PathVariable String nMatches){
         System.out.println("pinged getLastN");
 
-        return service.fetchLastNMatches(nMatches);
+        List<ETH_USD_MATCH> matches = service.fetchLastNMatches(nMatches);
+        System.out.println("RETURNING: \n" + matches);
+        return matches;
+//        return "{\"key\":\"value\"}";
     }
 }
